15a78a3be9198d27f2eff2dd20b6634367e55460
Aris Darmawan <arisrooney.09@gmail.com>
2020-04-25 18:18:53 +0700

    Create main.tf

---
+++ backend-s3/main.tf
@@ -0,0 +1,41 @@
+resource "aws_kms_key" "state_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 30
+}
+
+resource "aws_s3_bucket" "remote_state" {
+  bucket = "alpha-remote-state-zxcvbnm"
+  acl    = "private"
+  region = "ap-southeast-1"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name = "terraform remote state"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.state_key.id}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+output "bucket_arn" {
+  value       = "${aws_s3_bucket.remote_state.arn}"
+  description = "ARN of remote state bucket"
+}
+
+output "bucket_id" {
+  value       = "${aws_s3_bucket.remote_state.id}"
+  description = "ID of remote state bucket"
+}
