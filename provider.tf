928a81019ade09be1591e4004714de1d5271a7ad
Aris Darmawan <arisrooney.09@gmail.com>
2020-04-25 18:10:07 +0700

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+	region     = "${var.aws_region}"
+}
+
+terraform {
+   backend "s3" {
+    bucket = "wordpress-terraform"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {}
+© 2020 GitHub, Inc.
