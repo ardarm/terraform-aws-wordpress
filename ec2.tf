2aeb04465ef9ae1da3e625223673ce3719c4a9cb
Aris Darmawan <arisrooney.09@gmail.com>
2020-04-25 18:16:12 +0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -22,3 +22,8 @@ data "template_file" "bootstrap" {
         efsid = "${aws_efs_file_system.wordpressfs.id}"
   }
 }
+
+resource "aws_key_pair" "wordpress" {
+  key_name   = "wordpress"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYpQzI4ptEd2xu2FNS7FWiPq92FjINUHDnpgKrxsZntITrm3XSYvSpLjN1Nn/9pTpi85brV+qjj5UnSgAdzCbuLRCGv85Q+rGMRQJ0HRslvjaVZz+EmwEjo3itUh+vP5gEP4ehASDvEodPCMdSQOoVETo+UYoUN2vxA1U1U7icdb7Xiq93DYtUXlppBn1U3t5Yhi2P+S9qZBNYv6C7fB2qM3qTIF6Kch2WMiWNs3A1Zvs5qgdp6MkBDgKc7/9T468/jqyH9MdVF2OqKVmF20t/tySOa0liFN2vy9aBF/7vTjAUjdLjFI1DS8+GbWE3Wi7SDXyKI6jZkdcO3fa+Iq0r aris@Ariss-MacBook-Pro.local"
+}
